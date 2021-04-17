645c412a2e4a9499d41d5e1da6b453217407b4df
tugbaca <tugbacan57@outlook.com>
2021-04-17 19:12:34 +0300

    date that contains the date when this piece of code was loaded. Date when this piece of code was loaded inside the tags <span id="load_date"> ... </span>.
    POST /api/code/new should take a JSON object with a single field code, use it as the current code snippet, and return an empty JSON. In the upcoming stages, it won't be empty.

--- src/platform/controller/Controller.java
+++ src/platform/controller/Controller.java
@@ -1,19 +1,21 @@
 package platform.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import platform.model.Code;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 
 public class Controller {
 
-    private final String titldeData = "Code";
+    private final String titleData = "Code";
     private final String codeData =  "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
 
-    private final Code code = new Code(titldeData,codeData);
+    private final Code code = new Code(titleData,codeData,getCurrentTime);
 
     public Controller() {}
 
@@ -24,9 +26,29 @@ public class Controller {
 
     @GetMapping(path = "/code",produces = "text/html")
     public ResponseEntity<String> getHtmlCode() {
-        return ResponseEntity.ok()
-                .body("<title>" + code.getTitle() + "<title>"
-                + "<pre>" + code.getCode() + "</pre>");
+        String sendCode = "<title>" + code.getTitle() + "</title>"
+                + "<body>"
+                + "<span id=\"load_date\">" + code.getDate() + "</span>"
+                + "<pre id=\"code_snippet\">" + code.getCode() + "</pre>"
+                + "</body>";
+
+        return ResponseEntity.ok().body(sendCode);
+    }
+
+    @PostMapping(value = "/api/code/new", consumes = "application/json; charset=utf-8")
+    public String setApiCode(@RequestBody Code recode) {
+        code.setCode(recode.getCode());
+        code.setTitle("Code");
+        code.setDate(getCurrentTime());
+
+        return "{}";
+    }
+
+    public String getCurrentTime(){
+        LocalDateTime today = new LocalDateTime.now();
+        DateTimeFormatter formatter = new DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        return today.format(formatter);
     }
 
 }
